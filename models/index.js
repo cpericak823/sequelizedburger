87e24c9668448389b9c527c4811a106889601425
Cassie <cassie.pericak@northwestern.edu>
2017-02-03 22:30:28 -0600

    added jawsdb

--- models/index.js
+++ models/index.js
@@ -10,8 +10,8 @@
     var config = require(__dirname + '/../config/config.json')[env];
     var db = {};
 
-    if (config.use_env_variable) {
-        sequelize = new Sequelize(process.env[config.use_env_variable]);
+    if (config.use_env.JAWSDB_URL) {
+        sequelize = new Sequelize(process.env[config.env.use_env_variable]);
     } else {
         sequelize = new Sequelize(config.database, config.username, config.password, config);
     }
